ef6bd9fe3f0d2dfef3711bf23118904876485f31
Dominic <74207434+DominicDern@users.noreply.github.com>
2025-07-10 18:35:21 -0300

    removed id system as it was causing issues

--- src/initiative_queue.rs
+++ src/initiative_queue.rs
@@ -3,32 +3,24 @@ use priority_queue::PriorityQueue;
 
 #[derive(Debug)]
 pub struct InitiativeQueue<'a> {
-    queue: PriorityQueue<(&'a Character, usize), u16>,
+    queue: PriorityQueue<&'a Character, u16>,
     position: usize,
-    current_id: usize,
 }
 
 impl<'a> InitiativeQueue<'a> {
     /// Creates a inititive queue with pre rolled inititive numbers
     pub fn new_pre_rolled(actors: Vec<(&'a Character, u16)>) -> Self {
         let mut queue: PriorityQueue<&Character, u16> = PriorityQueue::new();
-        let mut index = 0;
         for roll in actors {
             queue.push(roll.0, roll.1);
-            index += 1;
         }
-        let current_id = queue.len();
-        Self {
-            queue,
-            position: 0,
-            current_id,
-        }
+        Self { queue, position: 0 }
     }
 }
 
 impl<'a> InitiativeQueue<'a> {
     // TODO add rolling inititive
-    pub fn get_queue(&self) -> Option<Vec<((&'a Character, usize), u16)>> {
+    pub fn get_queue(&self) -> Option<Vec<(&'a Character, u16)>> {
         if self.queue.is_empty() {
             None
         } else {
@@ -42,7 +34,7 @@ impl<'a> InitiativeQueue<'a> {
     }
 
     pub fn add(&mut self, actor: &'a Character, initiative: u16) {
-        todo!()
+        self.queue.push(actor, initiative);
     }
 
     pub fn next_turn(&mut self) {
