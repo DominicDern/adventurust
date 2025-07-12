199820049f70cf2921c6e0bc563cba88b21500ac
Dominic <74207434+DominicDern@users.noreply.github.com>
2025-07-12 15:19:39 -0300

    started working on state and character id system for frontend
    will come back later with upgraded initiative queue system that uses ids to return the correct character

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,32 @@ mod health;
 mod initiative_queue;
 
 use character::Character;
+use health::Health;
 use initiative_queue::InitiativeQueue;
 
+struct State<'a> {
+    initiative_queue: Option<InitiativeQueue<'a>>,
+}
+
+enum Message {
+    SetHealth(u64, Health),
+}
+
+impl Message {
+    fn update(&mut self, message: Message) {
+        match message {
+            Self::SetHealth(character, health) => {}
+        }
+    }
+}
+
 fn main() {
+    let mut current_actor_id: u64;
+
     let aaron = Character::from_file("src/example_characters/aaron_nightbringer.toml");
     let lyra = Character::from_file("src/example_characters/lyra_shadebottom.toml");
     let jarn = Character::from_file("src/example_characters/jarn_baker.toml");
-    let mut queue = InitiativeQueue::new_pre_rolled(vec![(&aaron, 1)]);
-    queue.add(&jarn, 4);
-    queue.add(&lyra, 3);
+    let mut queue = InitiativeQueue::new_pre_rolled(vec![(&aaron, 1), (&lyra, 11), (&jarn, 10)]);
     println!("{:?}", queue.get_queue());
     queue.next_turn();
     println!("");
