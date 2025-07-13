4a9f9894dad8e31232c91f3de483b921e164c5f1
Dominic <74207434+DominicDern@users.noreply.github.com>
2025-07-13 12:16:41 -0300

    Swapped queue to ids and fixed errors in inserting new characters

--- src/main.rs
+++ src/main.rs
@@ -4,16 +4,20 @@ mod condition;
 mod health;
 mod initiative_queue;
 
+use std::collections::HashMap;
+
+use crate::character::ID;
+use actor::Actor;
 use character::Character;
 use health::Health;
 use initiative_queue::InitiativeQueue;
 
-struct State<'a> {
-    initiative_queue: Option<InitiativeQueue<'a>>,
+struct State {
+    initiative_queue: Option<InitiativeQueue>,
 }
 
 enum Message {
-    SetHealth(u64, Health),
+    SetHealth(ID, Health),
 }
 
 impl Message {
@@ -27,12 +31,21 @@ impl Message {
 fn main() {
     let mut current_actor_id: u64;
 
+    let mut ids = HashMap::new();
+
     let aaron = Character::from_file("src/example_characters/aaron_nightbringer.toml");
     let lyra = Character::from_file("src/example_characters/lyra_shadebottom.toml");
     let jarn = Character::from_file("src/example_characters/jarn_baker.toml");
-    let mut queue = InitiativeQueue::new_pre_rolled(vec![(&aaron, 1), (&lyra, 11), (&jarn, 10)]);
-    println!("{:?}", queue.get_queue());
-    queue.next_turn();
-    println!("");
+    ids.insert(aaron.clone().get_id(), aaron.clone());
+    ids.insert(lyra.clone().get_id(), lyra.clone());
+    ids.insert(jarn.clone().get_id(), jarn.clone());
+    let mut queue = InitiativeQueue::new_pre_rolled(vec![
+        (aaron.get_id(), 1),
+        (lyra.get_id(), 11),
+        // (jarn.get_id(), 10),
+    ]);
+    let mut queue = queue.unwrap();
+    // queue.add(lyra.get_id(), 11);
+    queue.add(jarn.get_id(), 10);
     println!("{:?}", queue.get_queue());
 }
